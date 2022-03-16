95f36fb9dc3f7009d236284e8cf66e7274a49ed8
jotaraz <33379677+jotaraz@users.noreply.github.com>
2022-03-16 18:13:30 +0100

    Add files via upload

--- SerielleSchnittstelle/SerielleSchnittstelle.ino
+++ SerielleSchnittstelle/SerielleSchnittstelle.ino
@@ -1,5 +1,6 @@
 //Definiert die Pins, an denen Motor etc. an den Arduino angeschlossen werden
 
+
 int dir = 2; //Direction Pin des Schrittmotors
 int ste = 3; //Step Pin des Schrittmotors
 int ena = 7;  //Enable Pin des Schrittmotors
@@ -12,18 +13,23 @@ int button = 6; //Pin für Hauptschalter
 
 
 int usDelay = 2000; //Delay zwischen HIGH und LOW für einen Schritt in Mikrosekunden;
+
+
 int k = 1; //Gibt Richtung an, kann 1 oder -1 sein
-int pos = 0;
+float pos = 0;
+//int steps = 100; //Schritte, die in jede Richtung in jedem Loop gemacht werden
+float schrittweite = 1;
+float posL = 0.0;
+float posR = 10; //140.0;
 
-int steps = 100; //Schritte, die in jede Richtung in jedem Loop gemacht werden
-
-
+//Hauptschalterzeug
 int  run = 1; //Gibt an, ob der Motor laufen darf, gegeben durch den Hauptschalter
 bool ts = false; //Tasterstatus des Buttons
 bool tsold = false; //Tasterstatus des Buttons in der letzten Iteration
 
-int val = 0;
-String s = "";
+//int val = 0;
+//String s = "";
+int n = 10; //Maximale Zeichenanzahl im Input
 
 void setup() {
   Serial.begin(9600);
@@ -54,7 +60,7 @@ void setup() {
   digitalWrite(m3, LOW);
 }
 
-
+//Setzt die Microsteps
 void Vollschritt() {
   digitalWrite(m1, LOW);
   digitalWrite(m2, LOW);
@@ -85,25 +91,23 @@ void Sechzehntelschritt() {
   digitalWrite(m3, HIGH);
 }
 
-void go(int steps) {
+bool go(int steps) { //Diese Methode lässt den Motor steps (Mikro)Schritte gehen, dss Vorzeichen entscheidet über die Richtung
   bool allowed = true;
   int i=0;
-  k = steps/abs(steps);
-  //Andere Vorzeichen als im anderen Programm
-  if(k==-1) {
-    digitalWrite(dir, HIGH); //Die Drehrichtung wird eingestellt und k daher geändert
+  k = steps/abs(steps); //Richtung, 1 oder -1
+  if(k==1) {
+    digitalWrite(dir, HIGH); //Die Drehrichtung wird eingestell, Uhrzeigersinn CW
   } else {
-    digitalWrite(dir, LOW); //Die Drehrichtung wird eingestellt und k daher geändert
+    digitalWrite(dir, LOW); //Die Drehrichtung wird eingestellt, Gegen den Uhrzeigersinn CCW
   }
-  while(allowed && i <= abs(steps)) {
+  while(allowed && i < abs(steps)) {
     allowed = check_LR();
     i++;
-    if(pos%5==0) {
-      Serial.print("Position ");
-      Serial.println(pos);  
-    }
   }
-  Serial.write("Ich bin angekommen");
+  if(!allowed) {
+    Serial.println("Weg wurde durch Schalter beendet");
+  }
+  return allowed;
 }
 
 
@@ -112,64 +116,19 @@ bool check_LR() { //Diese Funktion überprüft, ob die Endschalter gedrückt sin
   bool tsL = digitalRead(L); //Tasterstatus des linken bzw. rechten Endschalters
   bool tsR = digitalRead(R);
   if (run == 1) { //Darf sich nur drehen, wenn der Hauptschalter das erlaubt
-    if(k == 1 && tsL) { //Der Motor darf sich nur nach rechts drehen, wenn der rechte Endschalter es erlaubt
+    
+    if(k == 1 && tsR) { //Der Motor darf sich nur nach rechts drehen, wenn der rechte Endschalter es erlaubt
       do_step();
-      pos += 1;
+      pos += schrittweite;
       allowed = true;
-    } else if(k == -1 && tsR) { //Der Motor darf sich nur nach rechts drehen, wenn der rechte Endschalter es erlaubt
-      do_step();
-      pos -= 1;
-      allowed = true;
-    } 
-    if(tsL == false) {
-      Serial.println("Linker Schalter ist gedrückt");
     }
-    if(tsR == false) {
-      Serial.println("Rechter Schalter ist gedrückt");
+    if(k == -1 && tsL) { //Der Motor darf sich nur nach rechts drehen, wenn der rechte Endschalter es erlaubt
+      do_step();
+      pos -= schrittweite;
+      allowed = true;
     } 
    }
    return allowed;    
-}
-
-String* split(String input, char delimiter) {
-  Serial.println(input.length());
-  char a[input.length()];
-  input.toCharArray(a, input.length());
-  //Serial.println("a:");
-  //Serial.println(a);
-  
-  int num_del = 1;
-  for(int i=0; i<input.length(); i++) {
-    if(a[i] == delimiter) {
-      Serial.print(a[i]);
-      Serial.print(" : ");
-      Serial.println(i);
-      num_del += 1;
-    }
-  }
-
-  int count = 0;
-  if(num_del == 1) {
-    String parts[3] = {input, "", ""};
-  } else {
-    String* parts = new String[num_del];
-    
-    for(int i=0; i<input.length(); i++) {
-      if(a[i] != delimiter) {
-        parts[count] = parts[count]+a[i]; 
-      } else {
-        count += 1;
-      }
-    }
-    Serial.print("num_del = ");
-    Serial.println(num_del);
-    Serial.println(parts[0]);
-    Serial.println(parts[1]);
-    Serial.println(parts[2]);
-    Serial.println("-");
-    
-    return parts;
-  }
 }
 
 void do_step() { //Führt einen Schritt (bzw. Microstep) durch
@@ -180,63 +139,82 @@ void do_step() { //Führt einen Schritt (bzw. Microstep) durch
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
   if(Serial.available()==0) {
     //Serial.println("nichts");
   } else {
-    s = Serial.readString();
-    Serial.println(" ");
-    Serial.print("s: ");
-    Serial.println(s);
-    String* a = split(s, ' ');
-    
-    
-    if(a[0] == "n") {
-      Serial.println("Neue 0");
-      pos = 0;
-    }
-
-    if(a[0] == "go") {
-      if(a[1] == "to") {
-        go(a[2].toInt()-pos);
-      } else if(a[1] == "steps") {
-        go(a[2].toInt());
-      } else if(a[1] == "end") {
-        if(a[2] == "L") {
-          go(-10 - pos);
-        } else if(a[2] == "R") {
-          go(150 - pos);
-        }
+    String s = Serial.readString();
+    char a[n];
+    s.toCharArray(a, n);
+    if(a[0] == 'L') {
+      Serial.println("\n going to L");
+      bool allowed = true;
+      int steps = -100/schrittweite;
+      while(allowed) {
+        allowed = go(steps);
       }
-    }
+      
+    } else if (a[0] == 'R') {
+      Serial.println("\n going to R");
+      bool allowed = true;
+      int steps = 100/schrittweite;
+      while(allowed) {
+        allowed = go(steps);
+      }
+    } else if (a[0] == 'p') {
+      Serial.print("Position: ");
+      Serial.println(pos);
+    } else if (a[0] == 'n') {
+      pos = 0;
+      Serial.print("Position: ");
+      Serial.println(pos);
 
-    if(a[0] == "activate") {
+    } else if(a[0] == 'm') {
+      if(a[1] == '1') {
+        if(a[2] == '6') {
+          Sechzehntelschritt();
+          schrittweite = 1.0/16.0;
+          Serial.println("\n 1/16 Schritte");
+        } else {
+          Vollschritt();
+          schrittweite = 1.0;
+          Serial.println("\n ganze Schritte");
+        }
+      } else if(a[1] == '2') {
+        Halbschritt();
+        schrittweite = 0.5;
+          Serial.println("\n 1/2 Schritte");
+      } else if(a[1] == '4') {
+        Viertelschritt();
+        schrittweite = 0.25;
+          Serial.println("\n 1/4 Schritte");
+      } else if(a[1] == '8') {
+        Achtelschritt();
+        schrittweite = 0.125;
+          Serial.println("\n 1/8 Schritte");
+      }
+    
+    } else if (a[0] == 'a') {
       digitalWrite(ena, LOW);
-    } else if(a[1] == "deactivate") {
+    } else if (a[0] == 'd') {
       digitalWrite(ena, HIGH);
-    } else if(a[0] == "1") {
-      Vollschritt();
-    } else if(a[0] == "2") {
-      Halbschritt();
-    } else if(a[0] == "4") {
-      Viertelschritt();
-    } else if(a[0] == "8") {
-      Achtelschritt();
-    } else if(a[0] == "16") {
-      Sechzehntelschritt();
+    } else {
+      Serial.print("\n Ziel (Schritte): ");
+      Serial.println(s);
+      Serial.print("\n Start: ");
+      Serial.println(pos);
+      int steps = s.toInt()/schrittweite;
+      go(steps);  
+      Serial.print("Ende: ");
+      Serial.println(pos);
     }
 
-    
 
-    
-    //Serial.println(val);
-    //Serial.println(char(val));
-    //Serial.println("---");  
-    delay(10);
   }
-  
-
-  
-
-  
 }
+
+
+  
+
+  
